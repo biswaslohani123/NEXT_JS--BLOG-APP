1fc0de9bed59db01a1b51dfdd149f7e79c5c06d7
biswasuser <lohanibiswas1@gmail.com>
2025-12-07 07:35:03 +0545

    completed home page

--- blog-app/app/page.js
+++ blog-app/app/page.js
@@ -1,5 +1,6 @@
 
 import BlogList from "@/components/BlogList";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Image from "next/image";
 
@@ -8,6 +9,7 @@ export default function Home() {
     <>
       <Header/>
       <BlogList/>
+      <Footer/>
     </>
   );
 }
