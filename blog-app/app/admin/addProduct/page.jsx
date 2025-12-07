d61803d3ac4ba5eabe13a96bb3357cce458f98bc
biswasuser <lohanibiswas1@gmail.com>
2025-12-07 20:04:56 +0545

    completed add blog section fo admin

--- blog-app/app/admin/addProduct/page.jsx
+++ blog-app/app/admin/addProduct/page.jsx
@@ -1,12 +1,45 @@
 "use client"
 
-import React from 'react'
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React, { useState } from 'react'
 
 const page = () => {
+
+    const [image, setImage] = useState(false)
+
   return (
-    <div>
+    <>
+
+        <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+
+            <p className='text-xl'>Upload thumbnail</p>
+
+            <label htmlFor="image">
+                <Image className='mt-4 ' src={!image ?assets.upload_area: URL.createObjectURL(image)} width={140} height={70} alt=''/>
+            </label>
+            
+            <input onChange={(e) => setImage(e.target.files[0])} type="file" id='image' hidden required />
+            <p className='text-xl mt-4 '>Blog title</p>
+            <input className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Type here' required />
+
+            <p className='text-xl mt-4 '>Blog Description</p>
+            <textarea className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='write content here' rows={6} required />
+
+            {/* Category Selection Button */}
+
+            <p className='text-xl mt-4'>Blog Category</p>
+            <select name="category" className='w-40 mt-4 px-4 py-3 border text-gray-500' >
+
+                <option value="Startup">Startup</option>
+                <option value="Technology">Technology</option>
+                <option value="Lifestyle">Lifestyle</option>
+            </select>
+            <br />
+            <button type='submit' className='mt-8 w-40 h-12 bg-black text-white cursor-pointer active:text-amber-50 active:bg-gray-600'>Add</button>
+        </form>
       
-    </div>
+    </>
   )
 }
 
