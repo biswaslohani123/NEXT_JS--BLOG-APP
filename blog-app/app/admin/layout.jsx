12b2f66c1fe73406ff0e82519bb88f7df0497291
biswasuser <lohanibiswas1@gmail.com>
2025-12-08 11:20:02 +0545

    comple

--- blog-app/app/admin/layout.jsx
+++ blog-app/app/admin/layout.jsx
@@ -11,6 +11,7 @@ export default function Layout({children}){
             <div className="flex">
             <ToastContainer theme="dark"/>
                 <SideBar/>
+                
 
             <div className="flex flex-col w-full">
                 <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border border-b border-black ">
---
+++ blog-app/components/AdminComponents/BlogTableItem.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+const BlogTableItem = () => {
+  return (
+    <tr className='bg-white border-b '>
+        <th scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
+            
+        </th>
+    </tr>
+  )
+}
+
+export default BlogTableItem
